44d45d21583218505f58d94a372c41ea2b627ef2
huanchenhc <huanchen.hc@gmail.com>
2021-08-18 22:29:48 -0700

    using add. to commit files

---
+++ src/main/java/org/huan/myk8s/Myk8sApplication.java
@@ -0,0 +1,13 @@
+package org.huan.myk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myk8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myk8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/huan/myk8s/Myk8sApplicationTests.java
@@ -0,0 +1,13 @@
+package org.huan.myk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myk8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
